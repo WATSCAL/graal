82c69b94dd756d05ebf0b4dfac9d095b803cec06
Jian Yu <lkmcfj@outlook.com>
2025-06-05 15:55:11 -0400

    make members of TypeAnalysisResult final

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/BytecodeNode.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/BytecodeNode.java
@@ -69,6 +69,7 @@ import com.oracle.truffle.api.source.Source;
 import com.oracle.truffle.api.source.SourceSection;
 import com.oracle.truffle.espresso.EspressoLanguage;
 import com.oracle.truffle.espresso.analysis.liveness.LivenessAnalysis;
+import com.oracle.truffle.espresso.analysis.typehints.TypeAnalysisResult;
 import com.oracle.truffle.espresso.analysis.typehints.TypeHintAnalysis;
 import com.oracle.truffle.espresso.bytecode.MapperBCI;
 import com.oracle.truffle.espresso.classfile.ExceptionHandler;
@@ -557,7 +558,7 @@ public final class BytecodeNode extends AbstractInstrumentableBytecodeNode imple
     private final int reifiedTypesCnt;
     private final TypeHints.TypeA[][] instructionTypeArgHints;
 
-    private final TypeHintAnalysis typeHintAnalysis = null;
+    private final TypeAnalysisResult[] typeAnalysisRes;
 
     public BytecodeNode(MethodVersion methodVersion) {
         CompilerAsserts.neverPartOfCompilation();
@@ -583,9 +584,7 @@ public final class BytecodeNode extends AbstractInstrumentableBytecodeNode imple
             }
         }
 
-        if (this.reifiedTypesCnt > 0){
-            TypeHintAnalysis.analyze(methodVersion);
-        }
+        this.typeAnalysisRes = this.reifiedTypesCnt > 0 ? TypeHintAnalysis.analyze(methodVersion).getResAtBCI() : null;
 
         this.frameDescriptor = createFrameDescriptor(methodVersion.getMaxLocals() + this.reifiedTypesCnt, methodVersion.getMaxStackSize() + maxExtraStack);
         this.noForeignObjects = getLanguage().isImplicitInteropEnabled() ? Truffle.getRuntime().createAssumption("noForeignObjects") : Assumption.ALWAYS_VALID;
