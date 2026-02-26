5dfd1ef686e60ac6906751f50a074bdd6b7826d0
Jian Yu <lkmcfj@outlook.com>
2026-02-26 16:54:12 +0100

    collect class type params at MethodWithBytecodeNode

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/MethodWithBytecodeNode.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/MethodWithBytecodeNode.java
@@ -45,7 +45,9 @@ import com.oracle.truffle.espresso.analysis.typehints.TypeHintAnalysis;
 import com.oracle.truffle.espresso.classfile.attributes.reified.MethodTypeParameterCountAttribute;
 import com.oracle.truffle.espresso.classfile.attributes.reified.TypeHints;
 import com.oracle.truffle.espresso.impl.Method;
+import com.oracle.truffle.espresso.impl.ObjectKlass;
 import com.oracle.truffle.espresso.impl.SuppressFBWarnings;
+import com.oracle.truffle.espresso.runtime.staticobject.StaticObject;
 
 /**
  * {@link RootTag} node that separates the Java method prolog e.g. copying arguments to the frame,
@@ -56,28 +58,28 @@ import com.oracle.truffle.espresso.impl.SuppressFBWarnings;
  */
 @ExportLibrary(NodeLibrary.class)
 final class MethodWithBytecodeNode extends EspressoInstrumentableRootNodeImpl {
+    static private final byte[] EMPTY_BYTE_ARRAY = new byte[0];
 
     @Child AbstractInstrumentableBytecodeNode bytecodeNode;
-    @Children BytecodeNode[] specializations;
-    @CompilerDirectives.CompilationFinal(dimensions=2) private byte[][] cacheKeys = null;
-    private final FrameDescriptor frameDescriptor;
-    
-    @CompilerDirectives.CompilationFinal
-    private int typeParamCount = 0;
-    private TypeAnalysisResult[] analysis;
     private final Method.MethodVersion methodVersion;
+    private final FrameDescriptor frameDescriptor;
     private final boolean trivialBytecode;
+
+    @Children BytecodeNode[] specializations = null;
+    @CompilerDirectives.CompilationFinal(dimensions=2) private byte[][] cacheKeys = null;
     
-    @CompilerDirectives.CompilationFinal
-    public static final boolean SHOW_TYPEANALYSIS = false;
+    private final int methodTypeParamCount;
+    private final int classTypeParamCount;
+    private TypeAnalysisResult[] analysis = null;
 
     MethodWithBytecodeNode(BytecodeNode bytecodeNode) {
         super(bytecodeNode.getMethodVersion());
         this.bytecodeNode = bytecodeNode;
         this.methodVersion = bytecodeNode.getMethodVersion();
-        this.trivialBytecode = BytecodeNode.isTrivialBytecodes(methodVersion);
         this.frameDescriptor = bytecodeNode.getFrameDescriptor();
-        this.specializations = null;
+        this.trivialBytecode = BytecodeNode.isTrivialBytecodes(methodVersion);
+        this.methodTypeParamCount = 0;
+        this.classTypeParamCount = 0;
     }
 
     MethodWithBytecodeNode(Method.MethodVersion methodVersion) {
@@ -87,18 +89,20 @@ final class MethodWithBytecodeNode extends EspressoInstrumentableRootNodeImpl {
 
         CompilerAsserts.neverPartOfCompilation();
         MethodTypeParameterCountAttribute attr = methodVersion.getMethod().getMethodTypeParameterCountAttribute();
-        this.typeParamCount = attr != null ? attr.getCount() : 0;
+        this.methodTypeParamCount = attr != null ? attr.getCount() : 0;
 
         this.analysis = TypeHintAnalysis.analyze(methodVersion);
         if (this.analysis != null) {
             this.bytecodeNode = null;
             this.frameDescriptor = BytecodeNode.calcFrameDescriptor(methodVersion);
+            this.classTypeParamCount = methodVersion.getMethod().hasReceiver() ? methodVersion.getDeclaringKlass().getLinkedKlass().getAllTypeParamNum() : 0;
             this.specializations = new BytecodeNode[0];
             this.cacheKeys = new byte[0][];
         } else {
             BytecodeNode t = new BytecodeNode(methodVersion, null, new byte[]{});
             this.bytecodeNode = t;
             this.frameDescriptor = t.getFrameDescriptor();
+            this.classTypeParamCount = 0;
         }
     }
 
@@ -122,10 +126,11 @@ final class MethodWithBytecodeNode extends EspressoInstrumentableRootNodeImpl {
             this.bytecodeNode.initializeFrame(frame);
             return this.bytecodeNode.execute(frame);
         }
-        byte[] key = collectReifiedValues(args);
+        byte[] methodTypeParams = collectMethodTypeParams(args);
+        byte[] classTypeParams = needClassTypeParams ? collectClassTypeParams((StaticObject) args[0]) : EMPTY_BYTE_ARRAY;
         
         for (int i = 0; i < cacheKeys.length; i++){
-            if (Arrays.equals(cacheKeys[i], key)){
+            if (matchKey(i, methodTypeParams, classTypeParams)) {
                 specializations[i].initializeFrame(frame);
                 return specializations[i].execute(frame);
             }
@@ -133,35 +138,65 @@ final class MethodWithBytecodeNode extends EspressoInstrumentableRootNodeImpl {
 
         CompilerDirectives.transferToInterpreterAndInvalidate();
 
-        BytecodeNode newNode = insertSpecialization(key);
+        BytecodeNode newNode = insertSpecialization(methodTypeParams, classTypeParams);
         newNode.initializeFrame(frame);
         return newNode.execute(frame);
     }
 
-    private byte[] collectReifiedValues(Object[] args){
-        byte[] key = new byte[typeParamCount];
-        for (int i = 0; i < typeParamCount; i++){
-            key[i] = (byte) args[args.length - typeParamCount + i];
+    private byte[] collectMethodTypeParams(Object[] args) {
+        if (methodTypeParamCount == 0) {
+            return EMPTY_BYTE_ARRAY;
         }
-        //TODO: class type parameters
+        byte[] key = new byte[methodTypeParamCount];
+        for (int i = 0; i < methodTypeParamCount; i++){
+            key[i] = (byte) args[args.length - methodTypeParamCount + i];
+        }
         return key;
     }
 
-    private BytecodeNode insertSpecialization(byte[] key){
-        CompilerDirectives.transferToInterpreterAndInvalidate();
+    private byte[] collectClassTypeParams(StaticObject receiver) {
+        return ((ObjectKlass) receiver.getKlass()).getLinkedKlass().getSpecializedKeyAt(receiver.specializationIndex);
+    }
 
-        BytecodeNode node = new BytecodeNode(methodVersion, analysis, key);
+    @ExplodeLoop(kind = ExplodeLoop.LoopExplosionKind.FULL_UNROLL_UNTIL_RETURN)
+    private boolean matchKey(int idx, byte[] methodTypeParams, byte[] classTypeParams) {
+        for (int i = 0; i < methodTypeParamCount; ++i) {
+            if (cacheKeys[idx][i] != methodTypeParams[i]) {
+                return false;
+            }
+        }
+        for (int i = 0; i < classTypeParamCount; ++i) {
+            if (cacheKeys[idx][methodTypeParamCount + i] != classTypeParams[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private BytecodeNode insertSpecialization(byte[] methodTypeParams, byte[] classTypeParams) {
+        CompilerAsserts.neverPartOfCompilation();
+
+        byte[] classTypeParamsUntilCurLevel = new byte[classTypeParamCount];
+        for (int i = 0; i < classTypeParamCount; ++i) {
+            classTypeParamsUntilCurLevel[i] = classTypeParams[i];
+        }
+        BytecodeNode node = new BytecodeNode(methodVersion, analysis, methodTypeParams, classTypeParamsUntilCurLevel);
         node = this.insert(node);
 
         int len = specializations.length;
-        BytecodeNode[] newSpecializations = Arrays.copyOf(specializations, len + 1);
-        newSpecializations[len] = node;
-        this.specializations = newSpecializations;
-        cacheKeys = Arrays.copyOf(cacheKeys, len + 1);
-        cacheKeys[len] = key;
+        this.specializations = Arrays.copyOf(this.specializations, len + 1);
+        this.specializations[len] = node;
+        this.cacheKeys = Arrays.copyOf(this.cacheKeys, len + 1);
+        this.cacheKeys[len] = new byte[methodTypeParamCount + classTypeParamCount];
+        for (int i = 0; i < methodTypeParamCount; ++i) {
+            this.cacheKeys[len][i] = methodTypeParams[i];
+        }
+        for (int i = 0; i < classTypeParamCount; ++i) {
+            this.cacheKeys[len][methodTypeParamCount + i] = classTypeParams[i];
+        }
         notifyInserted(node);
 
-        return node;        
+        return node;
     }
 
     @Override
