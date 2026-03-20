335b22fff35c080e5391d36069dbda38f14068ab
AragornXu <2101909718@qq.com>
2026-03-20 02:23:37 -0400

    updating SimpleClasses test case

--- tests/src/bcGen/Classes/SimpleClasses.scala
+++ tests/src/bcGen/Classes/SimpleClasses.scala
@@ -22,10 +22,104 @@ object SimpleClassesTest {
 
   val simpleClasses = new SimpleClasses
 
+  /* Offset: 26, ClassCount: 1
+    Class 0: LocalVariableCount: 2, Indices: 0 1
+
+    bcGen.Classes.SimpleClassesTest.pair1 =
+    {
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$Pair$A: Byte = 73
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$Pair$B: Byte = 76
+      new SimpleClasses.this.SimpleClasses$Pair(
+        bcGen.Classes.SimpleClassesTest.simpleClasses(),
+        InvokeReturnType(Byte)
+          reifiedLocal$bcGen$Classes$SimpleClasses$Pair$A,
+        InvokeReturnType(Byte)
+          reifiedLocal$bcGen$Classes$SimpleClasses$Pair$B,
+        NoBoxingNeeded Int.box(42), "hello")
+    }
+   */
   val pair1 = new simpleClasses.Pair[Int, String](42, "hello")
-  val pair2 = new simpleClasses.IntDoublePair(10, 3.14) //need locals for this?
+  /* Offset: 57, ClassCount: 2
+    Class 0: LocalVariableCount: 2, Indices: 2 3 
+    Class 1: LocalVariableCount: 0, Indices: 
+    
+    bcGen.Classes.SimpleClassesTest.pair2 =
+    {
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$Pair$A: Byte = 73
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$Pair$B: Byte = 68
+      new SimpleClasses.this.SimpleClasses$IntDoublePair(
+        bcGen.Classes.SimpleClassesTest.simpleClasses(), 10, 3.14d)
+    }
+   */
+  val pair2 = new simpleClasses.IntDoublePair(10, 3.14)
+  /* Offset: 90, ClassCount: 2
+    Class 0: LocalVariableCount: 2, Indices: 5 6 
+    Class 1: LocalVariableCount: 1, Indices: 4
+
+    bcGen.Classes.SimpleClassesTest.pair3 =
+    {
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$GenericPair$A: Byte = 68
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$Pair$A: Byte = 68
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$Pair$B: Byte = 68
+      new SimpleClasses.this.SimpleClasses$GenericPair(
+        bcGen.Classes.SimpleClassesTest.simpleClasses(),
+        InvokeReturnType(Byte)
+          reifiedLocal$bcGen$Classes$SimpleClasses$GenericPair$A,
+        NoBoxingNeeded Double.box(3.14d), NoBoxingNeeded Double.box(2.71d)
+        )
+    }
+   */
   val pair3 = new simpleClasses.GenericPair[Double](3.14, 2.71)
+  /* Offset: 132, ClassCount: 2
+    Class 0: LocalVariableCount: 2, Indices: 8 9 
+    Class 1: LocalVariableCount: 1, Indices: 7
+
+    bcGen.Classes.SimpleClassesTest.pair4 =
+    {
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$GenericPair$A: Byte = 76
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$Pair$A: Byte = 76
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$Pair$B: Byte = 76
+      new SimpleClasses.this.SimpleClasses$GenericPair(
+        bcGen.Classes.SimpleClassesTest.simpleClasses(),
+        InvokeReturnType(Byte)
+          reifiedLocal$bcGen$Classes$SimpleClasses$GenericPair$A,
+      "foo", "bar")
+    }
+   */
   val pair4 = new simpleClasses.GenericPair[String]("foo", "bar")
+  /* Offset: 170, ClassCount: 3
+    Class 0: LocalVariableCount: 2, Indices: 12 13 
+    Class 1: LocalVariableCount: 1, Indices: 11 
+    Class 2: LocalVariableCount: 1, Indices: 10
+
+    bcGen.Classes.SimpleClassesTest.pair5 =
+    {
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$GenericPair2$A: Byte = 90
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$GenericPair$A: Byte = 90
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$Pair$A: Byte = 90
+      tracked/<reified-local> val 
+        reifiedLocal$bcGen$Classes$SimpleClasses$Pair$B: Byte = 90
+      new SimpleClasses.this.SimpleClasses$GenericPair2(
+        bcGen.Classes.SimpleClassesTest.simpleClasses(),
+        InvokeReturnType(Byte)
+          reifiedLocal$bcGen$Classes$SimpleClasses$GenericPair2$A,
+        NoBoxingNeeded Boolean.box(true),
+        NoBoxingNeeded Boolean.box(false))
+    }
+   */
   val pair5 = new simpleClasses.GenericPair2[Boolean](true, false)
   val testid = pair1.id[Int](42)[String]
 
