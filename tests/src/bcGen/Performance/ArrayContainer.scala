c0a01ecfeee58603a0483f830af3d2a9721d0882
AragornXu <2101909718@qq.com>
2026-05-21 17:34:22 -0400

    adding scala-library-reified.jar; updating classfiles; removed ArrayUtils

--- tests/src/bcGen/Performance/ArrayContainer.scala
+++ tests/src/bcGen/Performance/ArrayContainer.scala
@@ -4,7 +4,8 @@ class ArrayContainer[T](val storage: Array[T]) {
   def reduce(fn: BinaryOp[T]): T = {
     var state = storage(0)
     var i = 1
-    while (i < storage.length) {
+    val length = storage.length
+    while (i < length) {
       state = fn.apply(state, storage(i))
       i += 1
     }
