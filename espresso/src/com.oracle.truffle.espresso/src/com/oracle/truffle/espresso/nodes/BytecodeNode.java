730efad854f051730c8b7aba44816d343db9be31
Jian Yu <j96yu@uwaterloo.ca>
2026-07-21 07:49:55 -0400

    fix: newReferenceObject should have @ExplodeLoop; liveness analysis now consider NEW reading type args from local variables

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/BytecodeNode.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/BytecodeNode.java
@@ -1895,6 +1895,7 @@ public final class BytecodeNode extends AbstractInstrumentableBytecodeNode imple
         enterImplicitExceptionProfile();
     }
 
+    @ExplodeLoop
     private StaticObject newReferenceObject(VirtualFrame frame, int curBCI, Klass klass) {
         assert !klass.isPrimitive() : "Verifier guarantee";
         GuestAllocator.AllocationChecks.checkCanAllocateNewReference(getMethod().getMeta(), klass, true, this);
